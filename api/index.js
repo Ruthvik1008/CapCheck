766a7d49611c2eb529d7330541cfad7024c3e762
Ruthvik Ananthula <ruthvika42@gmail.com>
2020-03-12 19:39:01 -0500

    test

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ module.exports = async function (req, res) {
   password = req.body.password;
  
   const myres = await response(username, password);
-
+  res.statusCode = 200;
   res.send(myres);
 };
 
