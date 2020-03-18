3fbb71a45c68bc353bf4fc84e348481e6adafec5
Ruthvik Ananthula <ruthvika42@gmail.com>
2020-03-17 19:49:09 -0500

    blah

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express()
+const app = express();
 const puppeteer = require('puppeteer');
 var bodyParser = require('body-parser'); //used to parse JSON
 const getResponse = require('./response');
