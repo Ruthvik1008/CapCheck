78af891b45dd20c2c26f79cce5d35edbeb41ff7a
Ruthvik Ananthula <ruthvika42@gmail.com>
2020-03-17 20:32:33 -0500

    bad

--- api/index.js
+++ api/index.js
@@ -3,10 +3,11 @@ var bodyParser = require('body-parser'); //used to parse JSON
 const getResponse = require('../response');
 
 module.exports = async function (req, res) {
-  username = req.body.username;
-  password = req.body.password;
- 
-  const myres = await response(username, password);
+  const { body } = req
+  username = body.username;
+  password = body.password;
+  
+  const myres = await getResponse(username, password);
   res.statusCode = 200;
   res.send(myres);
 };
