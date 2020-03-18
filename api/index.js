f22222db9a2f84d24c3e45a396ecb56d03838354
Ruthvik Ananthula <ruthvika42@gmail.com>
2020-03-17 21:03:17 -0500

    why

--- api/index.js
+++ api/index.js
@@ -3,9 +3,9 @@ var bodyParser = require('body-parser'); //used to parse JSON
 const getResponse = require('../response');
 
 module.exports = async function (req, res) {
-  
-  username = req.body.username;
-  password = req.body.password;
+  const { body } = req
+  username = body.username;
+  password = body.password;
   
   const myres = await getResponse(username, password);
   res.statusCode = 200;
